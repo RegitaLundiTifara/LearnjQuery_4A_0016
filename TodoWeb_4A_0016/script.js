f0cc651fc9f9b69fa8ec98cf3d9ae3f402516f75
RegitaLundiTifara <regita.tifara24@student.uisi.ac.id>
2026-05-11 22:35:59 +0700

    menagambil DOM

---
+++ TodoWeb_4A_0016/script.js
@@ -0,0 +1,5 @@
+const inputTugas = document.getElementById("inputTugas");
+const btntambah = document.getElementById("btnTambah");
+const daftarTugas = document.getElementById("daftarTugas");
+const inputTanggal = document.getElementById("inputTanggal");
+const inputStatus = document.getElementById("inputStatus");

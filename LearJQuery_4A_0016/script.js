018d34094aca67c7c96c2d6fb09cc06b09977f7d
RegitaLundiTifara <regita.tifara24@student.uisi.ac.id>
2026-05-18 20:09:25 +0700

    mengubah pembuatan element list menggunakan jQuery

--- LearJQuery_4A_0016/script.js
+++ LearJQuery_4A_0016/script.js
@@ -14,3 +14,44 @@ $(document).ready(function () {
         const tugas = inputTugas.val();
         const tanggal = inputTanggal.val();
         const status = inputStatus.val();
+
+        if (tugas === "" || tanggal === "") {
+            alert("Data harus dimasukkan!");
+            return;
+        }
+
+        if (dataEdit) {
+
+            dataEdit.find(".nama").html(tugas);
+            dataEdit.find(".tanggal").html("Tanggal: " + tanggal);
+
+            const elStatus = dataEdit.find(".status");
+
+            elStatus.html("Status: " + status);
+
+            if (status === "Done") {
+                elStatus.removeClass("progress").addClass("done");
+            } else {
+                elStatus.removeClass("done").addClass("progress");
+            }
+
+            dataEdit = null;
+            btnTambah.html("Tambah Data");
+
+    } else {
+        const listbaru = $(`
+                <li>
+                    <div class="info">
+                        <h3 class="nama">${tugas}</h3>
+                        <p class="tanggal">Tanggal: ${tanggal}</p>
+                        <p class="status ${status === "Done" ? "done" : "progress"}">
+                            Status: ${status}
+                        </p>
+                    </div>
+
+                    <div class="aksi">
+                        <button class="edit">Edit</button>
+                        <button class="hapus">Hapus</button>
+                    </div>
+                </li>
+            `);
